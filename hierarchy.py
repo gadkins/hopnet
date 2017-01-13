53cb1b0790bf44338e5349de00108cd0536bc1f4
Grayson Adkins <grayson.w.adkins@gmail.com>
2017-01-13 14:45:35 -0800

    comments updated

--- hierarchy.py
+++ hierarchy.py
@@ -1,3 +1,29 @@
+""" Returns the parent nodes of a dict-based general tree with
+    root -1 and leaves None  
+    Example: tree = {-1: {0: {2: {6: None}},
+					      1: {3: {7: None,
+								  8: None,
+								  9: None,
+								  10: None,
+								  11: None},
+							  4: {12: None,
+						 		  13: None},
+							  5: {14: None,
+								  15: None,
+								  16: None}}}}
+"""
+def get_parents(tree, idx):
+    for sub in dict_generator(tree):
+        if idx in sub:
+    	    p = [item for item in sub if item is not None and item is not -1]
+    	    p.sort()
+    	    return [i for i in p if i < idx]
+
+
+""" Credit for the following function belongs to Valentin Bryukhanov
+    from stackoverflow.com article: 'Python: Recommended way to walk 
+    complex dictionary structures imported from JSON?' 
+"""
 def dict_generator(indict, pre=None):
     pre = pre[:] if pre else []
     if isinstance(indict, dict):
@@ -13,12 +39,4 @@ def dict_generator(indict, pre=None):
                 yield pre + [key, value]
     else:
         yield indict
-
-
-def get_parents(tree, idx):
-    for sub in dict_generator(tree):
-        if idx in sub:
-    	    p = [item for item in sub if item is not None and item is not -1]
-    	    p.sort()
-    	    return [i for i in p if i < idx]
 
